1b04d6bb7d60847691f9bdeab494ec95cae4ec3e
StevenCreates <steven@stevencreates.tech>
2021-04-06 22:20:08 -0600

    starter files

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import {
+  motion,
+  useViewportScroll,
+  useSpring,
+  useTransform,
+  AnimatePresence
+} from "framer-motion";
+import styled from 'styled-components';
+import { Span } from "./Span";
+
+
+const ProgressContainer = styled(motion.div)`
+    position: fixed;
+    top: 20px;
+    left: 20px;
+    width: 120px;
+    height: 120px;
+`;
+
+
+function App() {
+  const [currentPrecent, setCurrentPercent] = useState(null)
+
+  return (
+    <>
+      <ProgressContainer>
+        
+      </ProgressContainer>
+      <Span />
+    </>
+  );
+}
+
+export default App;
